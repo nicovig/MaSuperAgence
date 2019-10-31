4db257dddbcab13abdb44f8eaeed2564c8e16f20
nico <koalaviril@gmail.com>
2019-10-31 13:27:35 +0100

    3/16

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index(PropertyRepository $repository):Response{
+
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig', [
+            'current_menu' => 'accueil',
+            'properties' =>$properties
+        ]);
+    }
+
+}
