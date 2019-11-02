2e2d4876ea44650bce9d3a8aa0665d4bc308b608
nico <koalaviril@gmail.com>
2019-11-02 18:45:05 +0100

    4

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,106 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminPropertyController extends AbstractController {
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em){
+
+    $this->repository = $repository;
+    $this->em = $em;
+
+    }
+
+    /**
+     * @Route ("/admin", name="admin.property.index")
+     * @return Response
+     */
+    public function index(){
+
+        $properties = $this->repository->findAll();
+
+        return $this->render('admin/property/index.html.twig', [
+            'current_menu' => 'edit',
+            'properties' => $properties
+        ]);
+
+    }
+
+    /**
+     * @Route("/admin/property/create", name="admin.property/new")
+     */
+    public function new (Request $request){
+
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            //on persiste la nouvelle property
+            $this->em->persist($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien crée avec succès.');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'current_menu' => 'edit',
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route ("/admin/property/{id}", name="admin.property.edit", methods={"GET", "POST"})
+     * @return Response
+     */
+    public function edit(Property $property, Request $request){
+
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->em->flush();
+            $this->addFlash('success', 'Bien modifié avec succès.');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/edit.html.twig', [
+            'current_menu' => 'edit',
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route ("/admin/property/{id}", name="admin.property.delete", methods={"DELETE"})
+     * @return Response
+     */
+    public function delete(Property $property, Request $request){
+
+        if($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))){
+            $this->em->remove($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien supprimé avec succès.');
+        }
+        return $this->redirectToRoute('admin.property.index');
+    }
+
+}
